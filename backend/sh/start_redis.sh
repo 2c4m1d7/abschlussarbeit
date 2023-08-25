9860f397a51426a56024c1a94ebf63ea26c07c74
Alexander Schmidt <alexanderpochta@web.de>
2023-08-25 10:09:37 +0200

    Update start_redis.sh

--- backend/sh/start_redis.sh
+++ backend/sh/start_redis.sh
@@ -9,36 +9,21 @@ fi
 port=$1
 path=$2
 
-
+chmod 600 $path
 
 redis-server $path --port $port > /dev/null 2>&1 &
 
-chmod 600 $path
 
-max_attempts=10
-interval=0.1  
+
+max_attempts=5
+interval=0.2  
 attempt=1
 
-while [ $attempt -le $max_attempts ]; do
- if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null; then
-    echo "Port $port is already in use."
-    exit 0
-fi
-
-  sleep $interval
-  ((attempt++))
-done
-
-echo "Failed to establish connection to Redis instance within the given timeout."
-exit 1
-
-
-
 # while [ $attempt -le $max_attempts ]; do
-#   if redis-cli -p $port ping > /dev/null 2>&1; then
-#     echo "Redis instance is up and running."
+#  if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null; then
+#     echo "Port $port is already in use."
 #     exit 0
-#   fi
+# fi
 
 #   sleep $interval
 #   ((attempt++))
@@ -46,3 +31,18 @@ exit 1
 
 # echo "Failed to establish connection to Redis instance within the given timeout."
 # exit 1
+
+
+
+while [ $attempt -le $max_attempts ]; do
+  if redis-cli -h $REDIS_HOSTNAME -p $port ping > /dev/null 2>&1; then
+    echo "Redis instance is up and running."
+    exit 0
+  fi
+
+  sleep $interval
+  ((attempt++))
+done
+
+echo "Failed to establish connection to Redis instance within the given timeout."
+exit 1
