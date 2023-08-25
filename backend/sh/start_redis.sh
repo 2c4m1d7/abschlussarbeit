9bd56f362c8f13f1a3b012af9b45b86ca7c6f28f
Alexander Schmidt <alexanderpochta@web.de>
2023-08-25 10:04:40 +0200

    Update start_redis.sh

--- backend/sh/start_redis.sh
+++ backend/sh/start_redis.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <port> <path>"
+    exit 1
+fi
+
+
 port=$1
 path=$2
 
@@ -13,10 +20,10 @@ interval=0.1
 attempt=1
 
 while [ $attempt -le $max_attempts ]; do
-  if redis-cli -p $port ping > /dev/null 2>&1; then
-    echo "Redis instance is up and running."
+ if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null; then
+    echo "Port $port is already in use."
     exit 0
-  fi
+fi
 
   sleep $interval
   ((attempt++))
@@ -24,3 +31,18 @@ done
 
 echo "Failed to establish connection to Redis instance within the given timeout."
 exit 1
+
+
+
+# while [ $attempt -le $max_attempts ]; do
+#   if redis-cli -p $port ping > /dev/null 2>&1; then
+#     echo "Redis instance is up and running."
+#     exit 0
+#   fi
+
+#   sleep $interval
+#   ((attempt++))
+# done
+
+# echo "Failed to establish connection to Redis instance within the given timeout."
+# exit 1
